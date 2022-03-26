ca8d6ea7c253312e875e224b13a6b478edf6742d
Ashish Thakkar <ashishvthakkar@gmail.com>
2022-03-26 15:20:37 -0400

    Simple rust comparison example

---
+++ rc/simple_monitor.cc
@@ -0,0 +1,31 @@
+#include <glog/logging.h>
+
+#include <chrono>
+#include <future>
+#include <thread>
+
+void RunWithMonitor() {
+  const auto num_tasks = 5;
+  std::atomic<int> tasks_completed = 0;
+  std::vector<std::future<void>> futures;
+  futures.reserve(num_tasks);
+  for (int i = 0; i < num_tasks; i++) {
+    futures.push_back(
+        std::async(std::launch::async, [num_tasks, &tasks_completed, i]() {
+          const auto processing_time = i + 1;
+          std::this_thread::sleep_for(std::chrono::seconds(processing_time));
+          tasks_completed++;
+        }));
+  }
+  while (tasks_completed < num_tasks) {
+    LOG(INFO) << tasks_completed << " / " << num_tasks << " complete";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+  }
+  LOG(INFO) << "All tasks complete.";
+}
+
+int main() {
+  LOG(INFO) << "Simple progress monitor.";
+  RunWithMonitor();
+  return 0;
+}
