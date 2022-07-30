c51d3452d7d6dabddff28263df16dc392b81c1db
Ashish Thakkar <ashishvthakkar@gmail.com>
2022-07-30 15:13:21 -0400

    Update variable name

--- rc/spsc_q.cc
+++ rc/spsc_q.cc
@@ -5,12 +5,12 @@
 int main() {
   LOG(INFO) << "Simple single producer single consumer demo.";
   const int size = 3;
-  code_experiments::SpscQ<double> q(size);
+  code_experiments::SpscQ<double> queue(size);
   using code_experiments::SpscQErrorCode;
-  CHECK((q.Enqueue(10) == SpscQErrorCode::kOk));
-  CHECK((q.Enqueue(12) == SpscQErrorCode::kOk));
+  CHECK((queue.Enqueue(10) == SpscQErrorCode::kOk));
+  CHECK((queue.Enqueue(12) == SpscQErrorCode::kOk));
   double ret_val = 0;
-  CHECK(q.Dequeue(ret_val) == SpscQErrorCode::kOk);
+  CHECK(queue.Dequeue(ret_val) == SpscQErrorCode::kOk);
   LOG(INFO) << ret_val;
   return 0;
 }
